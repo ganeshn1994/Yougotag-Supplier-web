38ca975aae5a46527e93cb58c061e2c54d07f675
hemanthpradeepkumar <hemanth.prdp@gmail.com>
2018-08-07 16:41:12 +0530

    Datepicker added to company wise sales report page

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-import { ApiService } from '../api.service'
+import { ApiService } from '../api.service';
+import { CookieService } from 'ngx-cookie-service';
+import {BsDatepickerConfig} from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-navbar',
@@ -8,8 +10,13 @@ import { ApiService } from '../api.service'
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  datePickerConfig: Partial<BsDatepickerConfig>;
+  startDateInCompanyReport:any='';
+  endDateInCompanyReport:any='';
 
-  constructor(private router:Router, private apiservice:ApiService) { }
+  constructor(private router:Router,private apiservice:ApiService,private cookieService: CookieService) { 
+    this.datePickerConfig = Object.assign({},{containerClass:'theme-dark-blue',showWeekNumbers:false,dateInputFormat:'YYYY/MM/DD'});
+  }
 
   ngOnInit() {
   }
